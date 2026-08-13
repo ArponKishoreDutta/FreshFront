4bfea155059335dfb034809cb88d8074e1064b5b
ArponKishoreDutta <arpondutta50@gmail.com>
2026-08-13 06:25:59 +0600

    Add automatic InfinityFree deployment

--- config.php
+++ config.php
@@ -1,9 +1,10 @@
 <?php
 
-$db_name = "mysql:host=localhost;dbname=shop_db";
-$username = "root";
+$db_name = "mysql:host=sql206.infinityfree.com;port=3306;dbname=if0_42642077_shop_db;charset=utf8mb4";
+$username = "if0_42642077";
 $password = "";
 
 $conn = new PDO($db_name, $username, $password);
+$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
 ?>
